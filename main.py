bb4e45595d093c30b458a73d656f19ea2bc68265
djcnvesifdbnsieuf <105562818+djcnvesifdbnsieuf@users.noreply.github.com>
2022-05-14 22:45:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import streamlit as st
 #import numpy as np
 import pandas as pd
-#import plotly.graph_objects as go
+import plotly.graph_objects as go
 #import geopandas as gpd
 #import matplotlib.pyplot as plt
 #import folium
